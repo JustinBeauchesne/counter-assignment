c24e71660e2b7088556526e4f6bc4d45f766b07d
JustinBeauchesne <beauchesnej@ufl.edu>
2024-04-05 18:04:32 -0400

    Test Cases Complete

--- src/tests/Counter.test.js
+++ src/tests/Counter.test.js
@@ -1,8 +1,11 @@
 // import necessary react testing library helpers here
 // import the Counter component here
+import { fireEvent, render, screen } from '@testing-library/react';
+import Counter from '../components/Counter';
 
 beforeEach(() => {
   // Render the Counter component here
+    render(<Counter />);
 })
 
 test('renders counter message', () => {
@@ -11,12 +14,21 @@ test('renders counter message', () => {
 
 test('should render initial count with value of 0', () => {
   // Complete the unit test below based on the objective in the line above
+  const state = screen.getByTestId("count"); // returns object 
+  expect(state.textContent).toBe("0");
 });
 
 test('clicking + increments the count', () => {
   // Complete the unit test below based on the objective in the line above
+  const state = screen.getByTestId("count");
+  fireEvent.click(screen.getByText("+"));
+  expect(state.textContent).toBe("1");
 });
 
 test('clicking - decrements the count', () => {
   // Complete the unit test below based on the objective in the line above
+  const state = screen.getByTestId("count");
+  fireEvent.click(screen.getByText("-"));
+  expect(state.textContent).toBe("-1");
+
 });
